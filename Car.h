67393c58eaba5b02b9ac04529674b498371ce9e8
giolexx <giurgiualexandru004@gmail.com>
2024-05-13 13:55:28 +0300

    Added new class attributes, one method

--- Car.h
+++ Car.h
@@ -7,6 +7,7 @@
 
 #include <string>
 
+
 enum FuelType
 {
     Diesel = 0,
@@ -21,11 +22,19 @@ enum TransmissionType
     Manual
 };
 
-enum ChassisType
+enum BodyType
 {
     Suv = 0,
     Coupe,
     Sports,
+};
+
+enum Drivetrain
+{
+    AWD = 0,
+    FourWD,
+    RWD,
+    FWD,
 };
 
 class Car
@@ -39,17 +48,20 @@ class Car
 
         FuelType fuelType;
         TransmissionType transmissionType;
+        BodyType bodyType;
+        Drivetrain drivetrain;
 
-    std::string color = "White";
-    std::string brand;
-    std::string model;
-    std::string carName;
+        std::string VIN;
+        std::string color = "White";
+        std::string make;
+        std::string model;
 
     public:
         Car();
         Car(int horsePower, int carPrice, int productionYear, int kmsDriven, int motorSize,
-            enum FuelType, enum TransmissionType, std::string color, std::string brand, std::string model, std::string carName);
+            enum FuelType, enum TransmissionType, enum BodyType, enum Drivetrain, std::string VIN, std::string color, std::string make, std::string model);
         int GetHP();
+        friend std::ostream& operator<< (std::ostream& os, const Car&);
 };
 
 
