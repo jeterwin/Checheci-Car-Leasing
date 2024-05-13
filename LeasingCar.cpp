4022385121e0f1491286f4bb39095750e647a049
giolexx <giurgiualexandru004@gmail.com>
2024-05-13 15:05:18 +0300

    Added constructor

--- LeasingCar.cpp
+++ LeasingCar.cpp
@@ -15,3 +15,19 @@ LeasingCar::LeasingCar(int leasingPrice, int leasingPeriod)
     this -> leasingPeriod = leasingPeriod;
     this -> leasingPrice = leasingPrice;
 }
+
+LeasingCar::LeasingCar(int horsePower, int carPrice, int productionYear, int kmsDriven, int motorSize,
+                       enum FuelType fuelType, enum TransmissionType transmissionType,
+                               enum BodyType bodyType,enum Drivetrain drivetrain,
+                                       std::string VIN, std::string color,std::string make, std::string model,
+                                       int leasingPrice, int leasingPeriod)
+                       : Car(horsePower, carPrice, productionYear, kmsDriven, motorSize, fuelType, transmissionType,
+                             bodyType, drivetrain, VIN, color, make, model),
+                             leasingPrice(leasingPrice), leasingPeriod(leasingPeriod) {}
+
+void LeasingCar::print() {
+    std::cout << "Leasing Price: " << leasingPrice << "\n";
+    std::cout << "Leasing Period: " << leasingPeriod << "\n";
+    std::cout << *this;
+}
+
