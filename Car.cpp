75d2db8c481c40cc539f9b247eb4760142a83b3a
giolexx <giurgiualexandru004@gmail.com>
2024-05-13 13:53:50 +0300

    Overload operator<< for printing car specs

--- Car.cpp
+++ Car.cpp
@@ -10,16 +10,39 @@ Car::Car()
     horsePower = carPrice = productionYear = kmsDriven = motorSize = 0;
     fuelType = Electric;
     transmissionType = Manual;
-    color = brand = model = carName = "";
+    bodyType = Coupe;
+    drivetrain = AWD;
+    VIN = color = make = model = "";
 }
 
 Car::Car(int horsePower, int carPrice, int productionYear, int kmsDriven, int motorSize,
-    enum FuelType fuelType, enum TransmissionType transmissionType, std::string color, std::string brand, std::string model, std::string carName)
+    enum FuelType fuelType, enum TransmissionType transmissionType, enum BodyType bodyType, enum Drivetrain drivetrain, std::string VIN, std::string color, std::string make, std::string model)
             : horsePower(horsePower), carPrice(carPrice), productionYear(productionYear),
             kmsDriven(kmsDriven), motorSize(motorSize), fuelType(fuelType),
-            transmissionType(transmissionType), color(color), brand(brand), model(model), carName(carName) {}
+            transmissionType(transmissionType), bodyType(bodyType), drivetrain(drivetrain), VIN(VIN), color(color), make(make), model(model){}
+
+
+std::ostream& operator<<(std::ostream& os, const Car& car)
+{
+    os << "Make: "<< car.make << "\n";
+    os << "Model: "<< car.model << "\n";
+    os << "Price: "<< car.carPrice << "\n";
+    os << "Body Type: "<< car.bodyType << "\n";
+    os << "Color: "<< car.color << "\n";
+    os << "Year of Fabrication: " << car.productionYear << "\n";
+    os << "VIN: " << car.VIN << "\n";
+    os << "Mileage: " << car.kmsDriven << "\n";
+    os << "Fuel Type: " << car.fuelType << "\n";
+    os << "Transmission: " << car.transmissionType << "\n";
+    os << "Drivetrain: " << car.drivetrain << "\n";
+    os << "Capacity: " << car.motorSize << "\n";
+    os << "Horse Power: " << car.horsePower;
+
+    return os;
+}
 
 int Car::GetHP()
 {
     return this -> horsePower;
 }
+
