8c48389a96d37fc1b4bdc6b9fe0dd5adf5a3fc03
giolexx <giurgiualexandru004@gmail.com>
2024-05-15 20:31:20 +0300

    Header file for main

---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by giurg on 5/15/2024.
+//
+
+#ifndef CHECHECI_CAR_LEASING_MAIN_H
+#define CHECHECI_CAR_LEASING_MAIN_H
+
+class MainClass
+{
+public:
+    void MenuOptions();
+};
+
+#endif //CHECHECI_CAR_LEASING_MAIN_H
