54f50e813b18902be17c360f13881fd945fec1a7
giolexx <giurgiualexandru004@gmail.com>
2024-05-15 17:16:44 +0300

    Added string functions implementations to return enum value

--- Car.cpp
+++ Car.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include "Car.h"
 
+
 Car::Car()
 {
     horsePower = carPrice = productionYear = kmsDriven = motorSize = 0;
@@ -27,14 +28,14 @@ std::ostream& operator<<(std::ostream& os, const Car& car)
     os << "Make: "<< car.make << "\n";
     os << "Model: "<< car.model << "\n";
     os << "Price: "<< car.carPrice << "\n";
-    os << "Body Type: "<< car.bodyType << "\n";
+    os << "Body Type: "<< stringBodyType(car.bodyType) << "\n";
     os << "Color: "<< car.color << "\n";
     os << "Year of Fabrication: " << car.productionYear << "\n";
     os << "VIN: " << car.VIN << "\n";
     os << "Mileage: " << car.kmsDriven << "\n";
-    os << "Fuel Type: " << car.fuelType << "\n";
-    os << "Transmission: " << car.transmissionType << "\n";
-    os << "Drivetrain: " << car.drivetrain << "\n";
+    os << "Fuel Type: " << stringFuelType(car.fuelType) << "\n";
+    os << "Transmission: " << stringTransmissionType(car.transmissionType) << "\n";
+    os << "Drivetrain: " << stringDrivetrain(car.drivetrain) << "\n";
     os << "Capacity: " << car.motorSize << "\n";
     os << "Horse Power: " << car.horsePower;
 
@@ -49,5 +50,93 @@ std::string Car::getStatus()
 int Car::GetHP()
 {
     return this -> horsePower;
+}
+
+std::string stringTransmissionType(enum TransmissionType x)
+{
+    switch(x)
+    {
+        case 0:
+            return "Automatic";
+            break;
+
+        case 1:
+            return "Manual";
+            break;
+
+        default:
+            return "InvalidTransmissionType";
+    }
+}
+
+std::string stringFuelType(enum FuelType x)
+{
+    switch(x)
+    {
+        case 0:
+            return "Diesel";
+            break;
+
+        case 1:
+            return "Electric";
+            break;
+
+        case 2:
+            return "Hybrid";
+            break;
+
+        case 3:
+            return "Gasoline";
+            break;
+
+        default:
+            return "InvalidFuelType";
+    }
+}
+
+std::string stringBodyType(enum BodyType x)
+{
+    switch(x)
+    {
+        case 0:
+            return "Suv";
+            break;
+
+        case 1:
+            return "Coupe";
+            break;
+
+        case 2:
+            return "Sports";
+            break;
+
+        default:
+            return "InvalidBodyType";
+    }
+}
+
+std::string stringDrivetrain(enum Drivetrain x)
+{
+    switch(x)
+    {
+        case 0:
+            return "AWD";
+            break;
+
+        case 1:
+            return "4WD";
+            break;
+
+        case 2:
+            return "RWD";
+            break;
+
+        case 3:
+            return "FWD";
+            break;
+
+        default:
+            return "InvalidDrivetrain";
+    }
 }
 
